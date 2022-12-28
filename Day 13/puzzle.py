7f1bb6136f4a80aac759e3e6b137eaaaeec0bd84
Florian Affolter <faffolter@student.ethz.ch>
2022-12-28 14:51:08 +0100

    Day 13

--- Day 13/puzzle.py
+++ Day 13/puzzle.py
@@ -1,3 +1,4 @@
+import functools
 import itertools
 import json
 
@@ -18,36 +19,58 @@ def grouper(iterable, n, *, incomplete='fill', fillvalue=None):
         raise ValueError('Expected fill, strict, or ignore')
 
 
-def compare(a, b):
-    if isinstance(a, int) and isinstance(b, int):
-        if a < b:
-            return True
-        elif b < a:
+@functools.total_ordering
+class Packet:
+    def __init__(self, packet) -> None:
+        self.packet = packet
+    
+    def __eq__(self, __o: object) -> bool:
+        if not isinstance(__o, Packet):
             return False
+        return self.packet == __o.packet
+    
+    def __lt__(self, __o: object) -> bool:
+        if not isinstance(__o, Packet):
+            return NotImplemented
+        if isinstance(self.packet, int) and isinstance(__o.packet, int):
+            if self.packet < __o.packet:
+                return True
+            elif __o.packet < self.packet:
+                return False
+            else:
+                return None
+        elif isinstance(self.packet, list) and isinstance(__o.packet, list):
+            for c, d in zip(self.packet, __o.packet):
+                cmp = Packet(c) < Packet(d)
+                if cmp is not None:
+                    return cmp
+            if len(self.packet) < len(__o.packet):
+                return True
+            elif len(self.packet) > len(__o.packet):
+                return False
+            else:
+                return None
+        elif isinstance(self.packet, int):
+            return Packet([self.packet]) < Packet(__o.packet)
+        elif isinstance(__o.packet, int):
+            return Packet(self.packet) < Packet([__o.packet])
         else:
-            return None
-    elif isinstance(a, list) and isinstance(b, list):
-        for c, d in zip(a, b):
-            cmp = compare(c, d)
-            if cmp is not None:
-                return cmp
-        if len(a) < len(b):
-            return True
-        elif len(a) > len(b):
-            return False
-        else:
-            return None
-    elif isinstance(a, int):
-        return compare([a], b)
-    elif isinstance(b, int):
-        return compare(a, [b])
-    else:
-        raise ValueError
+            raise ValueError
 
 
 output = 0
 with open('input', 'r') as f:
-    for i, (a, b) in enumerate(grouper(map(json.loads, filter(lambda line: line.strip(), f)), 2, incomplete='strict'), 1):
-        if compare(a, b):
+    for i, (a, b) in enumerate(grouper(map(lambda data: Packet(data), map(json.loads, filter(lambda line: line.strip(), f))), 2, incomplete='strict'), 1):
+        if a < b:
             output += i
 print(output)
+
+with open('input', 'r') as f:
+    packets = list(map(lambda data: Packet(data), map(json.loads, filter(lambda line: line.strip(), f))))
+    divider1 = Packet([[2]])
+    divider2 = Packet([[6]])
+    packets += [divider1, divider2]
+    packets.sort()
+    idx1 = packets.index(divider1) + 1
+    idx2 = packets.index(divider2) + 1
+    print(idx1 * idx2)
